ac8b176f960ad976496fb995d90715cb356dfc6a
recursossauro <recursossauro@gmail.com>
2024-11-02 20:08:25 -0300

    Created description field on models.Task; Create a display task and create model TaskHistory.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskListView, display_tasks, delete_task, TaskCreateView
+from .views import TaskListView, display_tasks, delete_task, TaskCreateView, TaskDetailView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='index'),
-    path('display_tasks/', display_tasks, name='display_tasks'),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='task'),
     path('create_task/', TaskCreateView.as_view(), name='create_task'),
+    path('display_tasks/', display_tasks, name='display_tasks'),
     path('task/<int:id>/delete/', delete_task, name='delete_task'),
-    ]
+]
