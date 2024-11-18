6aa7aba6a81f42ad78f9a05ff4370f84216e0ebc
Leonardo Düchting de Abreu e Lima <recursossauro@gmail.com>
2024-11-17 21:40:26 -0300

    settings.pu Changed TIME_ZONE to 'America/Sao_Paulo'; Added USE_L10N = True;

--- gtd/settings.py
+++ gtd/settings.py
@@ -108,9 +108,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+LANGUAGES = [
+    ('en', 'English'),
+    ('pt-br', 'Português (Brasil)'),
+]
+
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
